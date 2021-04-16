2a3b2602c7e5f6d8b3f066fef9738eef6421b62b
97254 <veskol1@gmail.com>
2021-04-16 16:45:10 +0300

    added navigation fragments

--- app/src/main/java/com/vesko/secondproject/view/jobs/MainActivity.kt
+++ app/src/main/java/com/vesko/secondproject/view/jobs/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vesko.secondproject
+package com.vesko.secondproject.view.jobs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vesko.secondproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
