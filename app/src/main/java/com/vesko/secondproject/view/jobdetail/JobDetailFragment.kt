65aa950e2eeb47267f99481d96d97dd2382c3d95
97254 <veskol1@gmail.com>
2021-04-16 17:28:37 +0300

    finished detailFragment, and bug fixing

--- app/src/main/java/com/vesko/secondproject/view/jobdetail/JobDetailFragment.kt
+++ app/src/main/java/com/vesko/secondproject/view/jobdetail/JobDetailFragment.kt
@@ -5,16 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.vesko.secondproject.R
+import com.vesko.secondproject.viewmodel.JobViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class JobDetailFragment : Fragment() {
-
+    private val jobViewModel : JobViewModel by viewModels()
+    private var selectedJobId = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
+            selectedJobId = it.getString("job_id","")
+            jobViewModel.start(selectedJobId)
         }
     }
 
@@ -26,5 +35,25 @@ class JobDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_job_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val companyTextView = view.findViewById<TextView>(R.id.company_name_text_view)
+        val titleJobTextView = view.findViewById<TextView>(R.id.title_job_text_view)
+        val descriptionTextView = view.findViewById<TextView>(R.id.description_job_text_view)
+
+        val companyLogo = view.findViewById<ImageView>(R.id.company_logo_image)
+
+        jobViewModel.job.observe(viewLifecycleOwner, { job ->
+            companyTextView.text = job.company
+            titleJobTextView.text = job.title
+            val fixedDescription = HtmlCompat.fromHtml(job.description, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            descriptionTextView.text = fixedDescription
+
+            Glide.with(companyLogo.context)
+                .load(job.company_logo)
+                .apply(RequestOptions().circleCrop())
+                .into(companyLogo)
+        })
+    }
 }
--- app/src/main/java/com/vesko/secondproject/view/jobs/JobsFragment.kt
+++ app/src/main/java/com/vesko/secondproject/view/jobs/JobsFragment.kt
@@ -1,25 +1,30 @@
 package com.vesko.secondproject.view.jobs
-
-
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.vesko.secondproject.R
+import com.vesko.secondproject.model.JobPojo
+import com.vesko.secondproject.viewmodel.JobsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class JobsFragment : Fragment(), JobsAdapter.ClickEventHandler {
 
+    private val jobsViewModel: JobsViewModel by viewModels()
     private lateinit var jobsAdapter: JobsAdapter
 
     override fun onCreateView(
@@ -43,13 +48,41 @@ class JobsFragment : Fragment(), JobsAdapter.ClickEventHandler {
 
         recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = JobsAdapter(this@JobsFragment)
+            adapter = JobsAdapter( this@JobsFragment)
             jobsAdapter = adapter as JobsAdapter
         }
 
         searchButton.setOnClickListener {
+
+
+            val location = locationEditText.text.toString()
+            val description = descriptionEditText.text.toString()
+
+            if(location.isNotEmpty() && description.isNotEmpty()) {
+                hideKeyboard()
+                progressBar.visibility = View.VISIBLE
+                searchTextView.visibility = View.GONE
+                jobsViewModel.fetchJobs(location, description)
+            }
+            else
+                Toast.makeText(requireContext(),"Please enter location and description", Toast.LENGTH_SHORT).show()
         }
 
+
+        jobsViewModel.myJobs.observe(viewLifecycleOwner, { jobs ->
+            searchTextView.visibility = View.GONE
+            progressBar.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            if(jobs.isNotEmpty())
+                jobsAdapter.setJobList(jobs as ArrayList<JobPojo>)
+            else
+                Toast.makeText(requireContext(),"Try another search details", Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    private fun hideKeyboard(){
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 
 
